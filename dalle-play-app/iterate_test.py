1140e2c6b087e395dce9014e8b28a1f0b946da44
alextmutter <alextmutter@gmail.com>
2022-08-31 00:47:13 +0100

    added iteration feature

--- dalle-play-app/iterate_test.py
+++ dalle-play-app/iterate_test.py
@@ -3,7 +3,7 @@ import requests
 
 BASE = 'http://127.0.0.1:8008/iterate'
 TEST_CASE = {
-    'image_path': 'outputs/2022-08-30_22-59-01_cartoon dog on the moon/0.jpeg',
+    'image_path': 'outputs/2022-08-30_22-59-01_cartoon\ dog\ on\ the\ moon/0.jpeg',
     'num_iterations': 8
     }
 
